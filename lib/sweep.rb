e5a0015736df0356745086353245507e1c33515d
abenson <abenson@gmail.com>
2013-06-06 09:59:30 -0500

    Add copyright. Add basic config options.

--- lib/sweep.rb
+++ lib/sweep.rb
@@ -2,5 +2,16 @@
 module Sweep
 	APP_NAME = 'sweep'
 	VERSION = '0.0.0'
+	COPYRIGHT = "© 2013"
+	Config = { 
+		:HostInterval => 10, 
+		:PortInterval => 5,
+		:AliveScanners => 10,
+		:TCPScanners => 10,
+		:UDPScanners => 5,	
+		:AliveModule => 'AliveICMP',
+		:TCPModule => 'TCPSyn',
+		:UDPModule => 'UDPCheck'
+	}
 end
 
