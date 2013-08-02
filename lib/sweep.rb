618f6cdb67a390304d48fb43066cfb3dce59b452
abenson <abenson@gmail.com>
2013-08-02 09:37:27 -0500

    Remove old Config variable. Superceded by Config class.

--- lib/sweep.rb
+++ lib/sweep.rb
@@ -3,16 +3,6 @@ module Sweep
 	APP_NAME = 'sweep'
 	VERSION = '0.0.0'
 	COPYRIGHT = "(C) Andrew Benson and contributors 2013"
-	Config = { 
-		:HostInterval => 10, 
-		:PortInterval => 5,
-		:AliveScanners => 10,
-		:TCPScanners => 10,
-		:UDPScanners => 5,	
-		:AliveModule => 'AliveICMP',
-		:TCPModule => 'TCPSyn',
-		:UDPModule => 'UDPCheck'
-	}
 end
 
 require 'rubygems'
